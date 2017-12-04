c7091f41feb48bb85de3f6c6a4e9880e1a21289f
lulili778 <32834159+lulili778@users.noreply.github.com>
2017-12-05 03:20:01 +0800

    Create ItemContract.java

---
+++ ItemContract.java
@@ -0,0 +1,20 @@
+package com.example.shiping.materialtest.Data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Contract class for database that defines essential fields: database name, database version,
+ * table name, and columns' names.
+ */
+public class ItemContract {
+    public static final String DB_NAME = "com.example.shiping.materialtest.db";
+    public static final int DB_VERSION = 1;
+    public static final String TABLE = "packing_list";
+
+    public class Columns {
+        public static final String ITEM = "items";
+        public static final String CHECKED = "checked";
+        public static final String _ID = BaseColumns._ID;
+    }
+
+}
